694d78d6a5f3a507c30c0b574d024451689c5536
Alfred Gomes <alfredgomes23@users.noreply.github.com>
2023-11-08 12:19:04 +0600

    database connected.

--- index.js
+++ index.js
@@ -2,11 +2,54 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+//dotenv
+require('dotenv').config();
+
+//db
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.by2eb1n.mongodb.net/?retryWrites=true&w=majority`;
+
 
 //middleware
-app.use(cors());
 app.use(express());
+app.use(cors());
+//custom middleware
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        //my db collections
+        
+        
+
+        //all featured-rooms
+        
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+//root route
 app.get('/', (req, resp) => {
     resp.send("Stars Hotel is Running.");
 });
